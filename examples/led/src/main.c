92f38d226d9b0a844edbdf2a46a3c6c580a4a73e
jura <jglotovyn@gmail.com>
2026-01-11 17:08:25 +0200

    ADD: test for gpio close

--- examples/led/src/main.c
+++ examples/led/src/main.c
@@ -13,9 +13,25 @@ int main (void)
     Led_t* ledGreen = Board_GetLed(BOARD_LED_GREEN);
     Led_t* ledYellow = Board_GetLed(BOARD_LED_YELLOW);
 
+/* NOTE: this is done for test only */
+#if 1
+    LedOn(ledWhite);
+    LedOn(ledGreen);
+    LedOn(ledYellow);
+
+    LedOff(ledWhite);
+    LedOff(ledGreen);
+    LedOff(ledYellow);
+
+    LedDeinit(ledWhite);
+    LedDeinit(ledGreen);
+    LedDeinit(ledYellow);
+
+    Board_Init();
+#endif
+
     while (1)
     {
-
         LedToggle(ledWhite);
         LedToggle(ledGreen);
         LedToggle(ledYellow);
