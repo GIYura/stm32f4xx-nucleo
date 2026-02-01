3be76c69b688fe6a9aaa3d304d37b576903c6f8e
jura <jglotovyn@gmail.com>
2026-02-01 15:17:24 +0200

    ADD: two buttons with debounce

--- examples/button/bsp/stm32f4xx/board.c
+++ examples/button/bsp/stm32f4xx/board.c
@@ -5,20 +5,48 @@
 #include "button.h"
 #include "gpio.h"
 #include "gpio-name.h"
+#include "timer.h"
+#include "sw-timer.h"
+
+#define HW_TIMER_TIMEOUT_MS             1
+#define BUTTON_DEBOUNCE_TIMEOUT_MS      20
 
 /* Button gpio */
 static GpioHandle_t m_gpioButtonUser;
+static GpioHandle_t m_gpioButtonExternal;
 
 /* Button */
 static Button_t m_buttonUser;
+static Button_t m_buttonExternal;
+
+/* Debounce timer (based on HW timer) */
+static SwTimer_t debounceTimer;
+static SwTimer_t debounceTimerExternal;
+
+/* HW timer */
+static TimerHandle_t m_hwTimer;
 
 void Board_Init(void)
 {
-    const GpioOps_t* ops = GpioGetOps();
+    const GpioOps_t* gpioOps = GpioGetOps();
+    const TimerOps_t* hwTimerOps = TimerGetOps();
 
-    m_gpioButtonUser.ops = ops;
+    m_gpioButtonUser.ops = gpioOps;
+    m_gpioButtonUser.ops->open(&m_gpioButtonUser, PC_13, PIN_MODE_INPUT, PIN_TYPE_PULL_UP, PIN_STRENGTH_HIGH, PIN_CONFIG_PUSH_PULL, PIN_STATE_HIGH);
 
-    ButtonInit(&m_buttonUser, &m_gpioButtonUser, PC_13);
+    m_gpioButtonExternal.ops = gpioOps;
+    m_gpioButtonExternal.ops->open(&m_gpioButtonExternal, PC_2, PIN_MODE_INPUT, PIN_TYPE_PULL_UP, PIN_STRENGTH_HIGH, PIN_CONFIG_PUSH_PULL, PIN_STATE_HIGH);
+
+    ButtonInit(&m_buttonUser, &m_gpioButtonUser, &debounceTimer, BUTTON_DEBOUNCE_TIMEOUT_MS);
+    ButtonInit(&m_buttonExternal, &m_gpioButtonExternal, &debounceTimerExternal, BUTTON_DEBOUNCE_TIMEOUT_MS);
+
+    m_hwTimer.ops = hwTimerOps;
+
+    m_hwTimer.ops->open(&m_hwTimer, HW_TIMER_TIMEOUT_MS);
+
+    m_hwTimer.ops->interrupt(&m_hwTimer, SwTimerTick);
+
+    m_hwTimer.ops->start(&m_hwTimer);
 }
 
 Button_t* Board_GetButton(BOARD_BUTTON_ID id)
@@ -29,6 +57,10 @@ Button_t* Board_GetButton(BOARD_BUTTON_ID id)
     {
     case BOARD_BUTTON_USER:
         return &m_buttonUser;
+        break;
+
+    case BOARD_BUTTON_EXTERNAL:
+        return &m_buttonExternal;
         break;
 
     default:
--- examples/button/src/main.c
+++ examples/button/src/main.c
@@ -5,10 +5,16 @@
 #include "board.h"
 
 static uint32_t m_buttonUserCounter = 0;
+static uint32_t m_buttonExternalCounter = 0;
 
 static void OnButtonUser(void)
 {
     ++m_buttonUserCounter;
+}
+
+static void OnButtonExternal(void)
+{
+    ++m_buttonExternalCounter;
 }
 
 int main (void)
@@ -16,16 +22,12 @@ int main (void)
     Board_Init();
 
     Button_t* buttonUser = Board_GetButton(BOARD_BUTTON_USER);
+    Button_t* buttonExternal = Board_GetButton(BOARD_BUTTON_EXTERNAL);
 
     ButtonRegisterHandler(buttonUser, OnButtonUser);
+    ButtonRegisterHandler(buttonExternal, OnButtonExternal);
 
-    while (1)
-    {
-        if (m_buttonUserCounter > 5)
-        {
-            ButtonDeinit(buttonUser);
-        }
-    }
+    while (1);
 
     return 0;
 }
