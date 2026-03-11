53a1dd33aeea0f965fe8abffc787599ea582555e
jura <jglotovyn@gmail.com>
2026-03-11 12:23:28 +0200

    ADD: board alias

---
+++ examples/wi-fi/bsp/stm32f4xx/board-pin.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_PIN_H
+#define BOARD_PIN_H
+
+#include "gpio-name.h"
+#include "uart-name.h"
+
+/* Board LEDs */
+#define BOARD_LED_GREEN     PA_5
+#define BOARD_LED_YELLOW    PC_3
+#define BOARD_LED_WHITE     PC_4
+
+/* ESP8266 UART */
+#define BOARD_ESP_UART      UART_1
+
+#endif /* BOARD_PIN_H */
+
